b8f2941bfec5cda9b8b28975d5b857a5dd76204d
Elizabeth Sham <esham00@stuy.edu>
2018-09-26 21:58:04 -0400

    Working print for 1-D array

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,1 +1,19 @@
-
+public class ArrayDemo {
+  public static void main(String[] args) {
+    int [] newArray = new int [5];
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = 10 + 2 * i;
+    }
+    printArray(newArray);
+  }
+  public static void printArray(int[]ary){
+  for (int i = 0; i < ary.length; i++) {
+    if (i == ary.length - 1) {
+      System.out.print(ary[i]);
+    }
+    else {
+    System.out.print(ary[i] + ", ");
+  }
+    }
+  }
+}
