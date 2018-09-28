551d4958790eee0edc1a4017cea9e2ace736f7fb
Elizabeth Sham <esham00@stuy.edu>
2018-09-27 21:12:35 -0400

    Added working fill2DCopy function and had to print fill2DCopy out before fill2D bc fill2D directly altered the array

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,19 +2,22 @@ public class ArrayDemo {
   public static void main(String[] args) {
     int [] newArray = {2, 4, 6, 8, 10};
     int [][] twoD = new int [][]{
-      {0, 1, 2, 3, 4},
-      {0, 1, 2, 3, 4},
-      {0, 1, 2, 3, 4},
-      {0, 1, 2, 3, 4},
-      {0, 1, 2, 3, 4}
+      {0, 1, 2, 3, -4},
+      {0, 1, 2, 0, -4},
+      {1, 0, -2, 3, -4},
+      {1, 2, 0, -4, 0},
+      {-6, 1, 2, 3, -4}
     };
+
     System.out.print("One-D array printing \n");
     printArray(newArray);
     System.out.println("\nTwo-D array printing");
     printArray(twoD);
     System.out.print("\nCounting Zeros \n");
     System.out.println(countZeros2D(twoD));
-    System.out.println("Filling 2D with 1s and 3s");
+    System.out.println("Filling 2D with 1s and 3s (if negative)");
+    printArray(fill2DCopy(twoD));
+    System.out.println("\nFilling 2D with 1s and 3s (if row = column)");
     fill2D(twoD);
   }
   public static void printArray(int[]ary){
@@ -63,5 +66,21 @@ public class ArrayDemo {
      }
    }
    printArray(vals);
+ }
+ public static int [][] fill2DCopy(int [][] vals) {
+   int [][] brandNew;
+   brandNew = new int [vals.length] [];
+   for (int i = 0; i < vals.length; i++) {
+     brandNew[i] = new int[vals[i].length];
+     for (int j = 0; j < vals[i].length; j++) {
+      if (vals[i][j] < 0) {
+        brandNew[i][j] = 3;
+      }
+      else {
+        brandNew[i][j] = 1;
+      }
+     }
+   }
+   return brandNew;
  }
 }
